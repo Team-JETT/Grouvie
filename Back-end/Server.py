4747b7320317422df3920d95ba40da93cee307c8
Erkin Salih <es3315@ic.ac.uk>
2017-05-31 23:36:48 +0100

    Major changes, see below:
    DataParser, modified in order to encapsulate main work behind functions.
    DBManager, now supports upsert. Insert and Update SQL calls confirmed
    working (upsert hasnt been tested).
    Server, Flask web framework to be used to talk with the front end,
    implemented some parts, untested.

--- Back-end/Server.py
+++ Back-end/Server.py
@@ -1,127 +1,45 @@
-#!/usr/bin/python3
+from flask import Flask, jsonify, request
+from DBManager import DBManager
+from DataParser import DataParser
 
-from flask import Flask
-import requests
-import re
-
-MILE_TO_KM = 1.60934
-FILM_MATCH_REGEX = r"(?P<film>[^\(]*)"
-CINEMA_MATCH_REGEX = r"(?P<cinema>[^,]*)"
-
-FILMS = []
-TIMES = []
-CINEMAS = []
-CINEMA_IDS = []
-DISTANCES = []
-F_TO_CID = {}
-FCID_TO_TIMES = {}
 app = Flask(__name__)
 
-
-# TODO: PLEASE DON'T USE THESE METHODS TOO MUCH. THEY ACTUALLY QUERY THE API.
-# IF YOU'RE GOING TO USE A LOT, SAVE THE DATA YOURSELF FOR TESTING.
-class Server:
-    """
-    This init will start the web server and call
-    """
-    def __init__(self):
-        pass
-
-    """
-    Give this function a postcode and CINEMAS, CINEMA_IDS and DISTANCES lists are 
-    populated with
-    (up to) 5 results.
-    """
-    def get_cinemas_postcode(self, postcode):
-        film_names = requests.get(
-            "http://api.cinelist.co.uk/search/cinemas/postcode/{}".format(postcode))
-        self.parse_cinema_data(film_names)
+dbManager = None
+dParser = None
 
 
-    """
-    Give this function a longitude and latitude and CINEMAS, CINEMA_IDS and 
-    DISTANCES lists are populated with (up to) 5 results.
-    """
-    def get_cinemas_longlat(self, longitude, latitude):
-        film_names = requests.get(
-            "https://api.cinelist.co.uk/search/cinemas/coordinates/{}/{}".
-            format(longitude, latitude))
-        self.parse_cinema_data(film_names)
+@app.route("/")
+def homepage():
+    return "Why are you here?"
 
 
-    def parse_cinema_data(self, film_names):
-        cinemas = film_names.json()["cinemas"][:5]
-        for i in cinemas:
-            # Runs regex over cinemas to remove the location
-            CINEMAS.append(re.match(CINEMA_MATCH_REGEX, i['name']).group("cinema"))
-            CINEMA_IDS.append(i['id'])
-            # Converts distance from mile to km and rounds to 3dp
-            DISTANCES.append(round(i['distance'] * MILE_TO_KM, 3))
-
-    """
-    Give this function a cinema ID and day and we can populate FILMS with all 
-    film showings and times.
-    :param day: If no day provided, assume today.
-    """
-    def get_films(self, cinema_id):
-        films = requests.get(
-             "http://api.cinelist.co.uk/get/times/cinema/{}".format(cinema_id))
-        #print(films.json())
-        for i in films.json()['listings']:
-            filmname = i['title']
-            times = i['times']
-            FILMS.append(filmname)
-            TIMES.append(times)
-            if filmname in F_TO_CID:
-                F_TO_CID[filmname].append(cinema_id)
-            else:
-                F_TO_CID[filmname] = [cinema_id]
-            FCID_TO_TIMES[(filmname, cinema_id)] = times
-
-    @app.route("/")
-    def home_page(self):
-        return "Hello World!"
+@app.route("/insert", methods=["GET", "POST"])
+def insert():
+    dbManager.insert(request.get_json())
+    return
 
 
+@app.route("/update_member", methods=["GET", "POST"])
+def update():
+    dbManager.update_insert(request.get_json())
+    return
 
-class WebServer:
 
-    def __init__(self):
-        pass
+@app.route("/get_cinemas", methods=["GET", "POST"])
+def get_cinemas():
+    location = request.get_json()
+    cinemas = dParser.get_cinemas_latlong(location['latitude'],
+                                          location['longitude'])
+    return cinemas
 
-    """
-    Function that, given the name of the movie FILMNAME, returns FILMNAME, the
-    closest cinema that is showing FILMNAME, the distance from that cinema to
-    the user's postcode in kilometres, and the earliest showtime for FILMNAME
-    in the cinema. It is returned as a tuple in the order mentioned above.
-    """
-    def get_event_from_film(self, filmname):
-        cid = F_TO_CID[filmname][0]
-        i = CINEMA_IDS.index(cid)
-        cine_name = CINEMAS[i]
-        dist = DISTANCES[i]
-        showtime = FCID_TO_TIMES[(filmname, cid)][0]
-        return (filmname, cine_name, dist, showtime)
 
-    """
-    *****FOR DEBUGGING PURPOSES*****
-    Function for testing output received from passing FILMNAME into the
-    get_event_from_film method
-    """
-    def print_event_for_film(self, filmname):
-        print(get_event_from_film(self, filmname))
+@app.route("/get_films", methods=["GET", "POST"])
+def get_films():
+    pass
 
 
 if __name__ == "__main__":
-    server = Server()
-    server.get_cinemas_postcode("cf147bj")
-    for i in CINEMA_IDS:
-        server.get_films(i)
-    webserver = WebServer()
-    webserver.print_event_for_film('King Arthur - Legend of the Sword')
-    #print(CINEMA_IDS)
-    #print(CINEMAS)
-    #print(FILMS)
-    #print(TIMES)
-    #print(F_TO_CID)
-    #print(FCID_TO_TIMES)
+    global dbManager, dParser
+    dbManager = DBManager()
+    dParser = DataParser()
+    app.run()
