ff327faba2015929673b99624b3158fce5332047
Tarun Sabbineni <ts2615@imp.ac.uk>
2017-05-31 15:04:17 +0100

    Created a new project due to bugs in running on VM

---
+++ Grouvie2/app/src/main/java/jett_apps/grouvie/SelectFilm.java
@@ -0,0 +1,33 @@
+package jett_apps.grouvie;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+public class SelectFilm extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_film);
+
+        String[] showingFilmsArray = {"Guardians of the Galaxy Vol 2",
+                                        "Fast and Furious 8",
+                                        "Boss Baby",
+                                        "WonderWoman",
+                                        "Baywatch",
+                                        "Alien: Covenant",
+                                        "Pirates of the Caribbean"};
+
+        ListAdapter filmAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, showingFilmsArray);
+        ListView filmsListView = (ListView) findViewById(R.id.filmList);
+        filmsListView.setAdapter(filmAdapter);
+
+    }
+
+}
