2f38b2ef840ac135da87409851ea36aa57b7058b
Tarun Sabbineni <ts2615@imp.ac.uk>
2017-05-31 11:46:55 +0100

    Added getIntent for next activity

--- Grouvie/app/src/main/java/jett_apps/grouvie/SelectFilm.java
+++ Grouvie/app/src/main/java/jett_apps/grouvie/SelectFilm.java
@@ -1,11 +1,9 @@
 package jett_apps.grouvie;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class SelectFilm extends AppCompatActivity {
 
@@ -16,14 +14,17 @@ public class SelectFilm extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Intent intent = getIntent();
+
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
 }
