8639cabb5522165383552c9c9764378e81cdc759
Tanmay <tk915@ic.ac.uk>
2017-06-06 22:39:07 +0100

    Fixed error in server.py not using port variable

--- Back-end/Server.py
+++ Back-end/Server.py
@@ -38,4 +38,4 @@ if __name__ == "__main__":
     dbManager = DBManager()
     dParser = DataParser()
     port = int(environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=port)
