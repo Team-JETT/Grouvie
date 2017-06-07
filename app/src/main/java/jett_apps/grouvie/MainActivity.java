321d09a3dc0d476e3f5ef34b3bdde3d2000eb065
Tarun Sabbineni <ts2615@imperial.ac.uk>
2017-06-07 19:48:30 +0100

    Added button to transfer to landing page fort now

--- app/src/main/java/jett_apps/grouvie/MainActivity.java
+++ app/src/main/java/jett_apps/grouvie/MainActivity.java
@@ -1,6 +1,9 @@
 package jett_apps.grouvie;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -8,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void loginSuccessful(View view) {
+        Intent intent = new Intent(this, LandingPage.class);
+        startActivity(intent);
     }
 
 }
