1de95924ca960694128ea4b72506af6d4308171f
Tarun Sabbineni <ts2615@imperial.ac.uk>
2017-06-09 12:06:31 +0100

    Implemented film posters

--- app/src/main/java/jett_apps/grouvie/CustomFilmAdapter.java
+++ app/src/main/java/jett_apps/grouvie/CustomFilmAdapter.java
@@ -1,8 +1,11 @@
 package jett_apps.grouvie;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,12 +15,20 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.InputStream;
 import java.util.ArrayList;
 
 
 public class CustomFilmAdapter extends ArrayAdapter<Film>{
 
     private Context context;
+    private Bitmap b;
 
     public CustomFilmAdapter(@NonNull Context context, ArrayList<Film> films) {
         super(context, R.layout.custom_film_layout, films);
@@ -36,8 +47,59 @@ public class CustomFilmAdapter extends ArrayAdapter<Film>{
         filmTitle.setText(f.getFilmName());
 
         ImageView filmPoster = (ImageView) customView.findViewById(R.id.filmPoster);
-        Glide.with(context).load(f.getImageUrl()).load(filmPoster);
+        String imageUrl = f.getImageUrl();
+        Glide.with(context).load(imageUrl).into(filmPoster);
+//        b = downloadBitmap(imageUrl);
+//        filmPoster.setImageBitmap(b);
 
         return customView;
+    }
+
+    private Bitmap downloadBitmap(String url) {
+        // initilize the default HTTP client object
+        final DefaultHttpClient client = new DefaultHttpClient();
+
+        //forming a HttoGet request
+        final HttpGet getRequest = new HttpGet(url);
+        try {
+
+            HttpResponse response = client.execute(getRequest);
+
+            //check 200 OK for success
+            final int statusCode = response.getStatusLine().getStatusCode();
+
+            if (statusCode != HttpStatus.SC_OK) {
+                Log.w("ImageDownloader", "Error " + statusCode +
+                        " while retrieving bitmap from " + url);
+                return null;
+
+            }
+
+            final HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                InputStream inputStream = null;
+                try {
+                    // getting contents from the stream
+                    inputStream = entity.getContent();
+
+                    // decoding stream data back into image Bitmap that android understands
+                    b = BitmapFactory.decodeStream(inputStream);
+
+
+                } finally {
+                    if (inputStream != null) {
+                        inputStream.close();
+                    }
+                    entity.consumeContent();
+                }
+            }
+        } catch (Exception e) {
+            // You Could provide a more explicit error message for IOException
+            getRequest.abort();
+            Log.e("ImageDownloader", "Something went wrong while" +
+                    " retrieving bitmap from " + url + e.toString());
+        }
+
+        return b;
     }
 }
