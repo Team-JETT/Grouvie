4516c980fca52bb487d52c5c96b8ccc2ef9d6161
Tarun Sabbineni <ts2615@imperial.ac.uk>
2017-06-14 17:36:15 +0100

    Added functionality to update contacts

---
+++ app/src/main/java/jett_apps/grouvie/Friend.java
@@ -0,0 +1,60 @@
+package jett_apps.grouvie;
+
+public class Friend {
+    private String name;
+    private String phoneNum;
+    private boolean checked = false;
+
+    public Friend(String name, String phoneNum){
+        this.name = name;
+        this.phoneNum = phoneNum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+
+    public String toString() {
+        return name;
+    }
+    public void toggleChecked() {
+        checked = !checked;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Friend friend = (Friend) o;
+
+        return phoneNum.equals(friend.phoneNum);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return phoneNum.hashCode();
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+}
