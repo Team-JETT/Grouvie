b7ea8f80555d0b55b2a55cd9e1c3c2d48ccc5edf
Erkin Salih <es3315@ic.ac.uk>
2017-06-15 17:40:43 +0100

    Bug fix for print statement in get_user

--- Back-end/Server.py
+++ Back-end/Server.py
@@ -133,7 +133,7 @@ def get_user():
                  "name": user_data[1],
                  "latitude": user_data[2],
                  "longitude": user_data[3]}
-    print "USER: " + json_data
+    print "USER: " + json.dumps(json_data)
     stdout.flush()
     return json.dumps(json_data)
 
