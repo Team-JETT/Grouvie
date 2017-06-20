f3a2514391340956fced2ebbe3686f4593fd5660
Jay <jrm315@ic.ac.uk>
2017-06-20 01:09:50 +0100

    Opens google maps for directions.

--- app/src/main/java/jett_apps/grouvie/Views/ConfirmPlanView.java
+++ app/src/main/java/jett_apps/grouvie/Views/ConfirmPlanView.java
@@ -3,8 +3,11 @@ package jett_apps.grouvie.Views;
 import android.Manifest;
 import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.net.Uri;
 import android.provider.CalendarContract;
 import android.support.v4.app.ActivityCompat;
@@ -13,9 +16,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.List;
 
+import jett_apps.grouvie.HelperClasses.ProfileManager;
 import jett_apps.grouvie.HelperObjects.Plan;
 import jett_apps.grouvie.R;
 
@@ -35,7 +43,30 @@ public class ConfirmPlanView extends AppCompatActivity {
     }
 
     public void getDirections(View view) {
+        Geocoder geocoder = new Geocoder(view.getContext());
+        String postcode = ProfileManager.getPostcode(view.getContext());
 
+        int destinationLatitude = 0;
+        int destinationLongitude = 0;
+        int sourceLatitude = 0;
+        int sourceLongitude = 0;
+
+        LatLng cinema = getLatLngFromLocationName(view.getContext(), p.getSuggestedCinema());
+        LatLng home = getLatLngFromLocationName(view.getContext(), postcode);
+
+
+        String uri = "http://maps.google.com/maps?saddr="
+                     + sourceLatitude
+                     + ","
+                     + sourceLongitude
+                     + "(" + "Home" + ")&daddr="
+                     + destinationLatitude
+                     + ","
+                     + destinationLongitude
+                     + " (" + p.getSuggestedCinema() + ")";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+        intent.setPackage("com.google.android.apps.maps");
+        startActivity(intent);
     }
 
     public void bookTickets(View view) {
@@ -62,9 +93,29 @@ public class ConfirmPlanView extends AppCompatActivity {
                 calDate.getTimeInMillis());
 
         startActivity(intent);
-
-
     }
+
+    public LatLng getLatLngFromLocationName(Context context, String address) {
+        Geocoder geocoder = new Geocoder(context);
+        List<Address> addressList;
+        LatLng position = null;
+
+        try {
+            addressList = geocoder.getFromLocationName(address, 5);
+            if(addressList == null) {
+                return null;
+            }
+            Address location = addressList.get(0);
+            location.getLongitude();
+            location.getLatitude();
+            position = new LatLng(location.getLatitude(), location.getLongitude());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return position;
+    }
+
 
 
 }
