0f1d50a6d8e0ea37f1dc5727c01b28112817532e
Jay <jrm315@ic.ac.uk>
2017-06-20 19:17:02 +0100

    Colour changed.

--- app/src/main/java/jett_apps/grouvie/Adapters/CustomGroupAdapter.java
+++ app/src/main/java/jett_apps/grouvie/Adapters/CustomGroupAdapter.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -15,22 +16,30 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 import jett_apps.grouvie.HelperObjects.Friend;
+import jett_apps.grouvie.R;
 import jett_apps.grouvie.Views.GroupView;
 
 public class CustomGroupAdapter extends ArrayAdapter<Friend> {
 
     public CustomGroupAdapter(@NonNull Context context, ArrayList<Friend> friends) {
-        super(context, android.R.layout.simple_list_item_1, friends);
+        super(context, R.layout.custom_group_layout, friends);
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         Friend friend = getItem(position);
-        View view = super.getView(position, convertView, parent);
-        TextView text = (TextView) view.findViewById(android.R.id.text1);
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View view = inflater.inflate(R.layout.custom_group_layout, parent, false);
+
+        TextView text = (TextView) view.findViewById(R.id.friendName);
         text.setText(friend.getName());
-        text.setTextColor(Color.WHITE);
+
+        if(friend.hasAccepted()) {
+            text.setTextColor(Color.GREEN);
+        } else {
+            text.setTextColor(Color.RED);
+        }
         return view;
     }
 }
