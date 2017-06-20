6ddf43d31b406b6daf67d37bae32700075caddac
Tanmay <tk915@ic.ac.uk>
2017-06-20 16:06:55 +0100

    Now showing cinema website on book ticket

--- app/src/main/java/jett_apps/grouvie/Views/ConfirmPlanView.java
+++ app/src/main/java/jett_apps/grouvie/Views/ConfirmPlanView.java
@@ -1,30 +1,24 @@
 package jett_apps.grouvie.Views;
 
-import android.Manifest;
-import android.content.ContentResolver;
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.net.Uri;
-import android.provider.CalendarContract;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.model.LatLng;
 
 import java.io.IOException;
-import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.ExecutionException;
 
-import jett_apps.grouvie.HelperClasses.ProfileManager;
+import jett_apps.grouvie.HelperClasses.ServerContact;
 import jett_apps.grouvie.HelperObjects.Plan;
 import jett_apps.grouvie.R;
 
@@ -44,8 +38,8 @@ public class ConfirmPlanView extends AppCompatActivity {
     }
 
     public void getDirections(View view) {
-        String postcode = ProfileManager.getPostcode(view.getContext());
-
+//        String postcode = ProfileManager.getPostcode(view.getContext());
+        String postcode = "SW10 0NJ";
         LatLng cinema = getLatLngFromLocationName(view.getContext(), p.getSuggestedCinema());
         LatLng home = getLatLngFromLocationName(view.getContext(), postcode);
 
@@ -65,7 +59,16 @@ public class ConfirmPlanView extends AppCompatActivity {
     }
 
     public void bookTickets(View view) {
-
+        String googleSearch = "https://www.google.co.uk/search?q=" + p.getSuggestedCinema();
+        String cinemaUrl = "";
+        try {
+            cinemaUrl = new ServerContact().execute("get_cinema_url", p.getSuggestedCinema()).get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        Uri uri = Uri.parse(cinemaUrl);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 
     public void addToCalendar(View view) {
