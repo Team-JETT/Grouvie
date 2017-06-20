5c9f1dda7d1eba11be27cf245a3238d6574467d0
Tarun Sabbineni <ts2615@imperial.ac.uk>
2017-06-20 21:23:11 +0100

    Updating UI very slightly to and shorter loading messages

--- app/src/main/java/jett_apps/grouvie/PlanningActivities/SelectGroup.java
+++ app/src/main/java/jett_apps/grouvie/PlanningActivities/SelectGroup.java
@@ -300,7 +300,7 @@ public class SelectGroup extends AppCompatActivity {
     public static void startProgressBar(Context context) {
         dialog = new ProgressDialog(context, ProgressDialog.BUTTON_POSITIVE);
         dialog.setTitle("Please wait");
-        dialog.setMessage("Refreshing Grouvie friends List from your phone contacts");
+        dialog.setMessage("Refreshing friends from contacts");
         dialog.show();
     }
 
