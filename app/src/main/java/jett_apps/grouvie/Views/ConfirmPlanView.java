31eee0f891d3716d0dd364c432c02adb7c3d6f94
Jay <jrm315@ic.ac.uk>
2017-06-20 16:42:10 +0100

    Added icons rather than a whole new view.

--- app/src/main/java/jett_apps/grouvie/Views/ConfirmPlanView.java
+++ app/src/main/java/jett_apps/grouvie/Views/ConfirmPlanView.java
@@ -43,72 +43,9 @@ public class ConfirmPlanView extends AppCompatActivity {
 
     }
 
-    public void getDirections(View view) {
-        String postcode = ProfileManager.getPostcode(view.getContext());
-
-        String uri = String.format(Locale.ENGLISH,"http://maps.google.com/maps?&saddr="
-                       + postcode
-                       + "&daddr="
-                       + p.getSuggestedCinema());
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-        intent.setPackage("com.google.android.apps.maps");
-        startActivity(intent);
-    }
-
     public void bookTickets(View view) {
 
     }
-
-    public void addToCalendar(View view) {
-        int day = p.getSuggestedDay();
-        int month = p.getSuggestedMonth();
-        int year = p.getSuggestedYear();
-
-        String time = p.getSuggestedShowTime();
-        String[] hourAndMinute = time.split(":");
-        int hour = Integer.parseInt(hourAndMinute[0]);
-        int minute = Integer.parseInt(hourAndMinute[1]);
-
-        Intent intent = new Intent(Intent.ACTION_INSERT);
-        intent.setData(CalendarContract.Events.CONTENT_URI);
-
-        intent.setType("vnd.android.cursor.item/event");
-        intent.putExtra(CalendarContract.Events.TITLE, "Grouvie Event: "+ p.getSuggestedFilm());
-        intent.putExtra(CalendarContract.Events.EVENT_LOCATION, p.getSuggestedCinema());
-        intent.putExtra(CalendarContract.Events.DESCRIPTION, p.getSuggestedFilm());
-
-        GregorianCalendar calDateStart = new GregorianCalendar(year, month, day, hour, minute);
-        GregorianCalendar calDateEnd = new GregorianCalendar(year, month, day, hour+3, minute);
-
-        intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
-                calDateStart.getTimeInMillis());
-        intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
-                calDateEnd.getTimeInMillis());
-
-        startActivity(intent);
-    }
-
-    public LatLng getLatLngFromLocationName(Context context, String address) {
-        Geocoder geocoder = new Geocoder(context);
-        List<Address> addressList;
-        LatLng position = null;
-
-        try {
-            addressList = geocoder.getFromLocationName(address, 5);
-            if(addressList == null) {
-                return null;
-            }
-            Address location = addressList.get(0);
-            location.getLongitude();
-            location.getLatitude();
-            position = new LatLng(location.getLatitude(), location.getLongitude());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return position;
-    }
-
 
 
 }
