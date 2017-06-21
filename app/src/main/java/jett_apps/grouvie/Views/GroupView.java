bd64ff5aac07ab8f5b0f5e5d2679ba6e03fa5817
Jay <jrm315@ic.ac.uk>
2017-06-21 11:57:31 +0100

    GroupView should refresh every 20 seconds.

--- app/src/main/java/jett_apps/grouvie/Views/GroupView.java
+++ app/src/main/java/jett_apps/grouvie/Views/GroupView.java
@@ -3,6 +3,7 @@ package jett_apps.grouvie.Views;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -51,67 +52,76 @@ public class GroupView extends AppCompatActivity {
             confirmPlan.setVisibility(View.INVISIBLE);
         }
 
-        JSONObject json = new JSONObject();
-        String result;
-        JSONObject group_replies = null;
-        try {
-            json.accumulate("leader", p.getLeaderPhoneNum());
-            json.accumulate("creation_datetime", p.getCreationDateTime());
-            result = new ServerContact().execute("group_replies", json.toString()).get();
-            group_replies = new JSONObject(result);
-        } catch (JSONException | ExecutionException | InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        final ArrayList<Friend> chosenFriends = p.getEventMembers();
-        if (group_replies != null) {
-
-
-            for (int i = 0; i < chosenFriends.size(); i++) {
-                Friend friend = chosenFriends.get(i);
-                String phoneNum = friend.getPhoneNum();
-
-                String film = null;
-                String time = null;
-                String date = null;
-                String cinema = null;
-                boolean hasAccepted = false;
-
-                JSONObject changes = null;
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                JSONObject json = new JSONObject();
+                String result;
+                JSONObject group_replies = null;
                 try {
-                    changes = group_replies.getJSONObject(phoneNum);
-                    film = changes.getString("film");
-                    time = changes.getString("showtime");
-                    date = changes.getString("date");
-                    cinema = changes.getString("cinema");
-                    hasAccepted = changes.getBoolean("accepted");
-                } catch (JSONException e) {
+                    json.accumulate("leader", p.getLeaderPhoneNum());
+                    json.accumulate("creation_datetime", p.getCreationDateTime());
+                    result = new ServerContact().execute("group_replies", json.toString()).get();
+                    group_replies = new JSONObject(result);
+                } catch (JSONException | ExecutionException | InterruptedException e) {
                     e.printStackTrace();
                 }
 
-                friend.setFilmChange(film);
-                friend.setTimeChange(time);
-                friend.setDateChange(date);
-                friend.setCinemaChange(cinema);
-                friend.setHasAccepted(hasAccepted);
+                final ArrayList<Friend> chosenFriends = p.getEventMembers();
+                if (group_replies != null) {
+
+
+                    for (int i = 0; i < chosenFriends.size(); i++) {
+                        Friend friend = chosenFriends.get(i);
+                        String phoneNum = friend.getPhoneNum();
+
+                        String film = null;
+                        String time = null;
+                        String date = null;
+                        String cinema = null;
+                        boolean hasAccepted = false;
+
+                        JSONObject changes = null;
+                        try {
+                            changes = group_replies.getJSONObject(phoneNum);
+                            film = changes.getString("film");
+                            time = changes.getString("showtime");
+                            date = changes.getString("date");
+                            cinema = changes.getString("cinema");
+                            hasAccepted = changes.getBoolean("accepted");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        friend.setFilmChange(film);
+                        friend.setTimeChange(time);
+                        friend.setDateChange(date);
+                        friend.setCinemaChange(cinema);
+                        friend.setHasAccepted(hasAccepted);
+                    }
+
+                    p.setEventMembers(chosenFriends);
+                }
+
+                ListAdapter groupAdapter = new CustomGroupAdapter(GroupView.this, chosenFriends);
+
+                ListView groupListView = (ListView) findViewById(R.id.groupView);
+                groupListView.setAdapter(groupAdapter);
+
+
+                groupListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Friend f = chosenFriends.get(position);
+                        changesMadeByFriend(f);
+                    }
+                });
+
             }
-
-            p.setEventMembers(chosenFriends);
-        }
-
-        ListAdapter groupAdapter = new CustomGroupAdapter(GroupView.this, chosenFriends);
-
-        ListView groupListView = (ListView) findViewById(R.id.groupView);
-        groupListView.setAdapter(groupAdapter);
+        }, 20000);
 
 
-        groupListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Friend f = chosenFriends.get(position);
-                changesMadeByFriend(f);
-            }
-        });
     }
 
     public void changesMadeByFriend(Friend friend) {
